95221008335556e1d6f437f4b9ac0a3a28fe9b57
KlecioHenrique <kleciohenrique18@gmail.com>
2022-12-02 01:58:21 -0300

    novas telas

--- src/components/index5.js
+++ src/components/index5.js
@@ -3,10 +3,162 @@ import styles5 from './index5.module.css';
 
 function Listas(){
     return(
-        // <div className={styles5.container5}>
-            <h1>Listas</h1>
+
+        <div className={styles5.body}>
+            <h2>Listagem de Usuario</h2>
+            <button className={styles5.button}> adicionar </button>
+                <div class="container">
+                    <div class="table-responsive">
+                        <table class="table" width="1000px" text-align="center">
+                            <tr className={styles5.button}>
+                                <th>id </th>
+                                <th>nome </th>
+                                <th>email </th>
+                                <th>senha </th>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>1 </td>
+                                <td>marciel</td>
+                                <td>CMTEHC@gmail.com</td>
+                                <td>CMTech</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>2</td>
+                                <td>luiza</td>
+                                <td>CMTEHC@gmail.com</td>
+                                <td>CMTech</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>3 </td>
+                                <td>kleiton</td>
+                                <td>CMTEHC@gmail.com</td>
+                                <td>CMTech</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>3 </td>
+                                <td>jose</td>
+                                <td>CMTEHC@gmail.com</td>
+                                <td>CMTech</td>
+                            </tr>
+                            <tr className={styles5.button}>
+                                <td>4 </td>
+                                <td>jussara</td>
+                                <td>CMTEHC@gmail.com</td>
+                                <td>CMTech</td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+
+                <h2>Listagem de Organização</h2>
+            <button className={styles5.button}> adicionar </button>
+                {/* <table>  */}
+                <div class="container">
+                    <div class="table-responsive">
+                        <table class="table" width="1000px" text-align="center">
+                            <tr className={styles5.button}>
+                                <th>id </th>
+                                <th>empresa </th>
+                                <th>Segmento </th>
+                                <th>contato </th>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>0 </td>
+                                <td>CMTECH</td>
+                                <td>Tecnologia da informação</td>
+                                <td>810000-0000</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>1</td>
+                                <td>CMTECH </td>
+                                <td>Tecnologia da informação</td>
+                                <td>810000-0000</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>2 </td>
+                                <td>CMTECH</td>
+                                <td>desenvolvimento</td>
+                                <td>810000-0000</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>3 </td>
+                                <td>CMTECH</td>
+                                <td>desenvolvimento</td>
+                                <td>810000-0000</td>
+                            </tr>
+                            <tr className={styles5.button}>
+                                <td>4 </td>
+                                <td>CMTECH</td>
+                                <td>desenvolvimento</td>
+                                <td>810000-0000</td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+
+                <h2>Listagem de Grupos</h2>
+            <button className={styles5.button}> adicionar </button>
+                {/* <table>  */}
+                <div class="container">
+                    <div class="table-responsive">
+                        <table class="table" width="1000px" text-align="center">
+                            <tr className={styles5.button}>
+                                <th>id </th>
+                                <th>nome </th>
+                                <th>quantidade integrandes </th>
+                                <th>custo </th>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>1 </td>
+                                <td>a</td>
+                                <td>3</td>
+                                <td>R$10000.00</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>2</td>
+                                <td>b </td>
+                                <td>4</td>
+                                <td>R$10000.00</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>3 </td>
+                                <td>c</td>
+                                <td>4</td>
+                                <td>R$10000.00</td>
+                            </tr>
+
+                            <tr className={styles5.button}>
+                                <td>4 </td>
+                                <td>d</td>
+                                <td>5</td>
+                                <td>R$10000.00</td>
+                            </tr>
+                            <tr className={styles5.button}>
+                                <td>5 </td>
+                                <td>e</td>
+                                <td>3</td>
+                                <td>R$10000.00</td>
+                            </tr>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+
             
-        // </div>
     );
 }
 
--- src/components/index6.js
+++ src/components/index6.js
@@ -1,13 +1,28 @@
-import{Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import styles6 from './index6.module.css';
 
 function Chat(){
     return(
-        <div className={styles6.container5}>
-            <h1>Listas</h1>
-            
-        </div>
+        <div className={styles6.body6}>
+          <div className={styles6.container6}>
+            <div className={styles6.header6}>
+              <h1>WebChat</h1>
+            </div>
+            <div className={styles6.body6}>
+              <p className={styles6.message6}>Hello</p>
+              <p className={styles6.user_message6}>Hi</p>
+            </div>
+            <div className={styles6.fooder}>
+              <form className={styles6.form6}>
+                <input type="text" name="" placeholder="Digite aqui"/>
+                <button>Enviar</button>
+              </form>
+            </div>
+          </div>
+		  </div>
+
     );
+
 }
 
 export default Chat;
